bdd275d0523d21e7a490bacc8d898386eb56bffe
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2025-08-30 09:15:26 +0200

    added info on pack system version

--- pack.pl
+++ pack.pl
@@ -3,3 +3,4 @@ title('A library for manipulating Binary Decision Diagrams').
 version('4.3.1').
 author('Fabrizio Riguzzi','fabrizio.riguzzi@unife.it').
 download('https://github.com/friguzzi/bddem/releases/*.zip').
+pack_version(1).
